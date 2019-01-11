40cb29692127f4737be719daf0fd816b285f8d87
topy123456789 <topy_ices@outlook.com>
2019-01-11 10:13:59 +0700

    ss321

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mongojs = require('mongojs');
 
 var databaseUrl = 'mongodb://localhost/integration';
 var collections = ['SensorData'];
-var option = {"auth":{"user":"query","password":"09"}};
+var option = {"auth":{"user":"8eeup","password":"tgr2019"}};
 var connect = mongojs(databaseUrl, collections, option);
 
 module.exports = {
